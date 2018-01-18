1c3e35d919e4144ba1ff0f6a55fb1a563fdc2d52
khangvtran <vinhkhangtran09@gmail.com>
2018-01-18 14:36:26 -0800

    Adding git ignore

---
+++ lab2.py
@@ -0,0 +1,21 @@
+# lab 2: Analyze and plot scores
+# Name: 
+
+
+def readExcel(  ) :
+    pass
+
+def readCSV(   ) :
+    pass
+ 
+def readFile(   ) :
+    pass
+            
+def analyze(   ) :
+    pass
+
+def main() :
+    (arrH, arrO)= readFile('data1.xlsx', 'data2.csv','data3.csv')
+    analyze(arrH, arrO)
+
+main()
